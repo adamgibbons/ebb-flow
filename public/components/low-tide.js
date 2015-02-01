af0d8c94b57684de786b0478b93b0418b1c40134
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-01 10:53:07 -0800

    removes state from low tide component

--- public/components/low-tide.js
+++ public/components/low-tide.js
@@ -6,50 +6,15 @@ var TidePredictionsStore = require('../stores/tide-predictions-store');
 
 var LowTideList = React.createClass({
 
-  mixins: [ListenToStore],
-
-  stores: [TidePredictionsStore],
-
-  componentWillMount: function() {
-    TidePredictionsActionCreators.requestTidePredictions();
-  },
-  
-  getInitialState: function() {
-    return {
-      levels: ''
-    }
-  },
-
-  getStateFromStore: function() {
-    this.setState({
-      levels: TidePredictionsStore.getLowTidePredictions()
-    });
-  },
-
   render: function() {
-
-    var listItems = [];
-
-    if (this.state.levels) {
-      this.state.levels.forEach(function (prediction) {
-        listItems.push(<LowTideRow time={prediction.time} level={prediction.level} />);
-      });
-    }
-
     return (
       <div>
-        <p>Santa Cruz, California<br/>Low Tides</p>
-        <p className='date'>Monday, January 25th</p>
-        <ul className='tide-list'>{listItems}</ul>
-      </div>
-    );
-  }
-});
+        <h1>Low Tides</h1>
+        <h2>Santa Cruz, California</h2>
 
-var LowTideRow = React.createClass({
-  render: function() {
-    return (
-      <li className='tide-list-item'>{this.props.level} ft @ {this.props.time}</li>
+        <p>{this.props.prediction.dateString} at {this.props.prediction.time}</p>
+        <p>Level: {this.props.prediction.level}ft</p>
+      </div>
     );
   }
 });
