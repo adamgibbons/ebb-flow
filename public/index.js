8af3647aa27f965e4088645c7a20508c090efed1
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-01 10:54:53 -0800

    sets state in parent component; passes low tide predictions into low time component via props; handles swipe actions

--- public/index.js
+++ public/index.js
@@ -2,27 +2,48 @@ require('./utils/object-assign');
 
 var React = require('react');
 
+var ListenToStore = require('./utils/listen-to-store');
+var TidePredictionsStore = require('./stores/tide-predictions-store');
+var TidePredictionsActionCreators = require('./actions/tide-prediction-action-creators');
 var LowTide = require('./components/low-tide');
 var Swiper = require('react-swiper');
 
 React.initializeTouchEvents(true);
 
 var App = React.createClass({
+
+  mixins: [ListenToStore],
+
+  stores: [TidePredictionsStore],
+
+  getInitialState: function() {
+    return {
+      prediction: {}
+    };
+  },
+
+  getStateFromStore: function() {
+    this.setState({
+      prediction: TidePredictionsStore.getCurrentPrediction()
+    });
+  },
+
+  componentWillMount: function() {
+    TidePredictionsActionCreators.requestTidePredictions();
+  },
+
   render: function() {
     return (
-      <Swiper className='swipe-container full-height' onSwipeLeft={this.handleSwipeLeft} onSwipeRight={this.handleSwipeRight}>
-        <LowTide/>
+      <Swiper className='swipe-container full-height' onSwipeLeft={this.handleSwipeLeft}>
+        <LowTide prediction={this.state.prediction} />
       </Swiper>
     );
   },
 
-  handleSwipeRight: function(e) {
-    console.log(e);
-  },
-
-  handleSwipeLeft: function(e) {
-    console.log(e);
-  },
+  handleSwipeLeft: function() {
+    console.log("swiping left...");
+    TidePredictionsActionCreators.getNextLowTide();
+  }
 });
 
 React.render(<App/>, document.getElementById('app'));
