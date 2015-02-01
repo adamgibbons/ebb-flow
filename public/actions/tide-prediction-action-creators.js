06abd6fe111c96355eeaaaafc6c4c70f5e2107c6
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-01 10:52:44 -0800

    adds action creators

--- public/actions/tide-prediction-action-creators.js
+++ public/actions/tide-prediction-action-creators.js
@@ -4,18 +4,30 @@ var Dispatcher = require('../dispatcher/dispatcher');
 var TideApi = require('../utils/noaa-api-wrapper');
 
 module.exports = {
+  getNextLowTide: function() {
+    Dispatcher.handleViewAction({
+      type: ActionTypes.GET_NEXT_LOW_TIDE
+    });
+  },
+
+  getFirstLowTide: function() {
+    Dispatcher.handleViewAction({
+      type: ActionTypes.GET_FIRST_LOW_TIDE
+    });
+  },
+
   requestTidePredictions: function() {
     Dispatcher.handleViewAction({
       type: ActionTypes.REQUEST_TIDE_PREDICTIONS
     });
 
-    TideApi.getTodaysLowTides();
+    TideApi.requestTidePredictions();
   },
 
   receiveTidePredictions: function(predictions) {
     Dispatcher.handleViewAction({
       type: ActionTypes.RECEIVE_RAW_TIDE_PREDICTIONS,
       levels: predictions
-    })
+    });
   }
 };
