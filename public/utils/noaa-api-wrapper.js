c0e72d9712094a6035261b85192a2ed1cfbbf3c5
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-01 10:55:38 -0800

    tweaks dummy data

--- public/utils/noaa-api-wrapper.js
+++ public/utils/noaa-api-wrapper.js
@@ -1,11 +1,13 @@
 var tides = [
-  {time: '09:40 AM', 'level': '1.16'},
-  {time: '09:02 PM', 'level': '1.36'}
+  {time: '09:40 AM', level: '1.16', dateString: 'Monday, January 25th'},
+  {time: '09:02 PM', level: '1.36', dateString: 'Monday, January 25th'},
+  {time: '10:05 AM', level: '1.90', dateString: 'Tuesday, January 26th'},
+  {time: '10:50 PM', level: '0.82', dateString: 'Tuesday, January 26th'}
 ];
 
 module.exports = {
-  getTodaysLowTides: function() {
-    console.log("ActionCreators");
-    require('../actions/tide-prediction-action-creators').receiveTidePredictions(tides)
+  requestTidePredictions: function() {
+    require('../actions/tide-prediction-action-creators').receiveTidePredictions(tides);
+    require('../actions/tide-prediction-action-creators').getFirstLowTide();
   }
 };
