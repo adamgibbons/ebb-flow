b523d680d7de1d5f959a5026cfb67f21d7ac273d
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-01 10:53:27 -0800

    adds action types

--- public/constants/action-types.js
+++ public/constants/action-types.js
@@ -2,5 +2,7 @@ var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
   REQUEST_TIDE_PREDICTIONS: null,
-  RECEIVE_RAW_TIDE_PREDICTIONS: null
+  RECEIVE_RAW_TIDE_PREDICTIONS: null,
+  GET_NEXT_LOW_TIDE: null,
+  GET_FIRST_LOW_TIDE: null
 });
