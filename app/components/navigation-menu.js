c760866128e4d53f2acc2dd2813331705e1858b2
adamgibbons <adam.d.gibbons@gmail.com>
2015-02-20 09:38:24 -0800

    disables nav btns beyond first, last prediction

--- app/components/navigation-menu.js
+++ app/components/navigation-menu.js
@@ -1,7 +1,28 @@
-var React = require('react');
+var React = require('react/addons');
 var ActionCreators = require('../actions/action-creators');
+var ApplicationStore = require('../stores/application-store');
+var ListenToStore = require('../utils/listen-to-store');
 
 var NavigationMenu = React.createClass({
+
+  stores: [ApplicationStore],
+
+  mixins: [ListenToStore],
+
+  getInitialState: function() {
+    return {
+      predictionIndex: 0,
+      predictions: []
+    };
+  },
+
+  getStateFromStore: function() {
+    this.setState({
+      predictionIndex: ApplicationStore.getPredictionIndex(),
+      predictions: ApplicationStore.getPredictions()
+    });
+  },
+
   getPreviousPrediction: function() {
     ActionCreators.getPreviousPrediction();
   },
@@ -10,11 +31,40 @@ var NavigationMenu = React.createClass({
     ActionCreators.getNextPrediction();
   },
 
+  getPredictionsCount: function() {
+    if (!this.state.predictions.length) {
+      return 0;
+    }
+
+    return this.state.predictions[0].periods.length;
+  },
+
+  isLastPrediction: function() {
+    var lastPredictionIndex = this.getPredictionsCount() - 1;
+
+    return this.state.predictionIndex === lastPredictionIndex;
+  },
+
+  isFirstPrediction: function() {
+    return this.state.predictionIndex === 0;
+  },
+
   render: function() {
+
+    var prevBtnClasses = React.addons.classSet({
+      'btn': true,
+      'disabled': this.isFirstPrediction()
+    });
+
+    var nextBtnClasses = React.addons.classSet({
+      'btn': true,
+      'disabled': this.isLastPrediction()
+    });
+
     return (
       <nav className="navigation-menu">
-        <button className="btn" onClick={this.getPreviousPrediction}>Previous</button>
-        <button className="btn" onClick={this.getNextPrediction}>Next</button>
+        <button className={prevBtnClasses} onClick={this.getPreviousPrediction}>Previous</button>
+        <button className={nextBtnClasses} onClick={this.getNextPrediction}>Next</button>
       </nav>
     );
   }
